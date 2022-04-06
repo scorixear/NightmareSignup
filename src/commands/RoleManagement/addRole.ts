c3e5bc33a5a18ffbd01aa139e8009f83a99b47df
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2022-04-06 17:16:28 +0200

    added multiple adds to role

--- src/commands/RoleManagement/addRole.ts
+++ src/commands/RoleManagement/addRole.ts
@@ -20,9 +20,9 @@ export default class AddRole extends CommandInteractionHandle {
     super(
       'addrole',
       ()=>languageHandler.replaceArgs(languageHandler.language.commands.roles.add.description, [config.botPrefix]),
-      'addrole @Scorix camlann',
+      'addrole @Scorix Camlann\naddrole @Scorix Camlann,Grovekeeper',
       'RoleManagement',
-      'addrole <user> <zvzrole>',
+      'addrole <user> <zvzrole[s]>',
       commandOptions,
       true,
     );
@@ -36,25 +36,32 @@ export default class AddRole extends CommandInteractionHandle {
     }
     const user = interaction.options.getUser('user');
     const zvzrole = interaction.options.getString('zvzrole');
+    let zvzroles = [];
+    if(zvzrole.includes(",")) {
+      zvzroles = zvzrole.split(",");
+    } else {
+      zvzroles.push(zvzrole);
+    }
     const guild = interaction.guild;
     const channel = interaction.channel;
     const author = interaction.user;
     if(!PartyHandler.Roles) {
       await PartyHandler.updateComposition();
     }
-    let found: Role;
-    if(zvzrole === "Battlemount") {
-      found = new Role();
-    } else {
-      found = PartyHandler.Roles.find(role => role.RoleName === zvzrole || role.PriorityRole === zvzrole);
+    const nonExistent = [];
+    for(const role of zvzroles) {
+      if (role !== "Battlemount" && PartyHandler.Roles.find(r => r.RoleName === role || r.PriorityRole === role) === undefined) {
+        nonExistent.push(role);
+        break;
+      }
     }
-    if(!found) {
+    if(nonExistent.length > 0) {
       try {
         await interaction.reply(await messageHandler.getRichTextExplicitDefault({
           guild: interaction.guild,
           author: interaction.user,
           title: languageHandler.language.commands.roles.add.error.role_title,
-          description: languageHandler.replaceArgs(languageHandler.language.commands.roles.add.error.role_desc, [zvzrole]),
+          description: languageHandler.replaceArgs(languageHandler.language.commands.roles.add.error.role_desc, ["- "+ nonExistent.join("\n- ")]),
           color: 0xcc0000,
         }));
       } catch (err) {
@@ -63,28 +70,56 @@ export default class AddRole extends CommandInteractionHandle {
           channel,
           author,
           title: languageHandler.language.commands.roles.add.error.role_title,
-          description: languageHandler.replaceArgs(languageHandler.language.commands.roles.add.error.role_desc, [zvzrole]),
+          description: languageHandler.replaceArgs(languageHandler.language.commands.roles.add.error.role_desc, ["- " + nonExistent.join("\n- ")]),
           color: 0xcc0000,
         });
       }
       return;
     }
-    const added = await sqlHandler.addRole(user.id, zvzrole);
-    if (added) {
+    const addedRoles = [];
+    const ignoredRoles = [];
+    for (const role of zvzroles) {
+      if(await sqlHandler.addRole(user.id, role)) {
+        addedRoles.push(role);
+      } else {
+        ignoredRoles.push(role);
+      }
+    }
+    try {
       interaction.reply(await messageHandler.getRichTextExplicitDefault({
         guild: interaction.guild,
         author: interaction.user,
         title: languageHandler.language.commands.roles.add.title,
-        description: languageHandler.replaceArgs(languageHandler.language.commands.roles.add.successdesc, ['<@' + user.id + '>', zvzrole]),
+        description: languageHandler.replaceArgs(languageHandler.language.commands.roles.add.successdesc, ['<@' + user.id + '>']),
+        categories: [
+          {
+            title: languageHandler.language.commands.roles.add.success.added,
+            text: '- '+addedRoles.join('\n- '),
+          },
+          {
+            title: languageHandler.language.commands.roles.add.success.ignored,
+            text: '- '+ignoredRoles.join('\n- '),
+          },
+        ],
       }));
-    } else {
-      interaction.reply(await messageHandler.getRichTextExplicitDefault({
+    } catch {
+      await messageHandler.sendRichTextDefaultExplicit({
         guild: interaction.guild,
+        channel: interaction.channel,
         author: interaction.user,
-        title: languageHandler.language.commands.roles.add.error.title,
-        description: languageHandler.replaceArgs(languageHandler.language.commands.roles.add.error.description, ['<@' + user.id + '>', zvzrole]),
-        color: 0xcc0000,
-      }));
+        title: languageHandler.language.commands.roles.add.title,
+        description: languageHandler.replaceArgs(languageHandler.language.commands.roles.add.successdesc, ['<@' + user.id + '>']),
+        categories: [
+          {
+            title: languageHandler.language.commands.roles.add.success.added,
+            text: '- '+addedRoles.join('\n- '),
+          },
+          {
+            title: languageHandler.language.commands.roles.add.success.ignored,
+            text: '- '+ignoredRoles.join('\n- '),
+          },
+        ],
+      });
     }
   }
 }
