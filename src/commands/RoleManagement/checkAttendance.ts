3305c2b6fb245dba29beda3b29eea551b4d1f88b
Paul Keller <pkeller1@stud.hs-heilbronn.de>
2022-04-20 21:07:30 +0200

    added checkAttendance for specific Event

--- src/commands/RoleManagement/checkAttendance.ts
+++ src/commands/RoleManagement/checkAttendance.ts
@@ -5,13 +5,18 @@ import { CommandInteractionHandle } from '../../model/CommandInteractionHandle';
 import { SlashCommandStringOption, SlashCommandUserOption } from '@discordjs/builders';
 import { LanguageHandler } from '../../misc/languageHandler';
 import { ISqlHandler } from '../../interfaces/ISqlHandler';
+import dateHandler from '../../misc/dateHandler';
 
 declare const languageHandler: LanguageHandler;
 declare const sqlHandler: ISqlHandler;
 
 export default class CheckAttendance extends CommandInteractionHandle {
    constructor() {
-    const commandOptions: any[] = [];
+    const commandOptions: any[] = [
+      new SlashCommandStringOption().setName('event-name').setDescription(languageHandler.language.commands.attendance.event_name_desc).setRequired(false),
+      new SlashCommandStringOption().setName('event-date').setDescription(languageHandler.language.commands.attendance.event_date_desc).setRequired(false),
+      new SlashCommandStringOption().setName('event-time').setDescription(languageHandler.language.commands.attendance.event_time_desc).setRequired(false),
+    ];
     super(
       'checkattendance',
       ()=>languageHandler.replaceArgs(languageHandler.language.commands.attendance.description, [config.botPrefix]),
@@ -30,6 +35,61 @@ export default class CheckAttendance extends CommandInteractionHandle {
       return;
     }
     interaction.deferReply();
+
+    const eventName = interaction.options.getString('event-name');
+    const eventDate = interaction.options.getString('event-date');
+    const eventTime = interaction.options.getString('event-time');
+    let events;
+    let description;
+    let limit;
+    if(eventName || eventDate || eventTime) {
+      if (!eventName || !eventDate || !eventTime) {
+        await interaction.reply({
+          content: languageHandler.language.commands.attendance.missing_parameter,
+          ephemeral: true,
+        });
+        return;
+      } else {
+        let eventTimestamp: number;
+        try {
+          const date = dateHandler.getDateFromUTCString(eventDate, eventTime);
+          eventTimestamp = dateHandler.getUTCTimestampFromDate(date);
+          if (isNaN(eventTimestamp)) {
+            await interaction.reply({
+              content: languageHandler.language.commands.attendance.error.formatDesc,
+              ephemeral: true,
+            });
+            return;
+          }
+        } catch (err) {
+          console.error(err);
+          await interaction.reply({
+            content: languageHandler.language.commands.deletesignup.error.formatDesc,
+            ephemeral: true,
+          });
+          return;
+        }
+        const eventId = await sqlHandler.getEventId(eventName, eventTimestamp.toString());
+        if (!eventId) {
+          await interaction.reply({
+            content: languageHandler.language.commands.attendance.error.event_not_found,
+            ephemeral: true,
+          });
+          return;
+        }
+        events = [{
+          id: eventId,
+          date: eventTimestamp,
+        }];
+        description = languageHandler.replaceArgs(languageHandler.language.commands.attendance.description_event, [eventName, eventDate, eventTime]);
+        limit = 0;
+      }
+    } else {
+      events = await sqlHandler.findEventObjects('9999999999');
+      description = languageHandler.language.commands.attendance.success.description;
+      limit = 1;
+    }
+
     const users: {userid: string, register: number}[] = (await sqlHandler.getUsers())
     const distinctUsers = new Map<string, number>();
     for(const user of users) {
@@ -39,7 +99,6 @@ export default class CheckAttendance extends CommandInteractionHandle {
         distinctUsers.set(user.userid, user.register);
       }
     }
-    const events = await sqlHandler.findEventObjects('9999999999');
     const userCounts = new Map<string, {reacted: number, max: number}>();
 
     for(const user of distinctUsers.keys()) {
@@ -71,7 +130,7 @@ export default class CheckAttendance extends CommandInteractionHandle {
     }
     const lines = new Array<string>();
     for(const pair of userCounts) {
-      if(pair[1].max - pair[1].reacted > 1) {
+      if(pair[1].max - pair[1].reacted > limit) {
         let member;
         try {
           member = await discordHandler.fetchMember(pair[0], interaction.guild);
@@ -91,7 +150,7 @@ export default class CheckAttendance extends CommandInteractionHandle {
       author: interaction.user,
       channel: interaction.channel,
       title: languageHandler.language.commands.attendance.success.title,
-      description: languageHandler.language.commands.attendance.success.description,
+      description,
       categories,
     });
   }
